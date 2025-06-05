374ab6856a8198459f292616ebd7bf5b2866a1a3
Alessio-Civitella <alessio.civitella19@gmail.com>
2025-06-05 11:59:48 +0200

    feat: creazione pagina Favourites per visualizzare e rimuovere carni preferite con localStorage

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,4 +1,42 @@
-const Favourites = () => {
-  return <div>Pagina favoriti</div>;
-};
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Favourites.css";
+import type { Carne } from "../types"
+
+function Favourites() {
+  const [favourites, setFavourites] = useState<Carne[]>([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("favourites");
+    if (saved) {
+      setFavourites(JSON.parse(saved));
+    }
+  }, []);
+
+  const removeFavourite = (id: string) => {
+    const updated = favourites.filter((carne) => carne.id !== id);
+    setFavourites(updated);
+    localStorage.setItem("favourites", JSON.stringify(updated));
+  };
+
+  if (favourites.length === 0) {
+    return <p>Non hai carni preferite.</p>;
+  }
+
+  return (
+    <div className="favourites-container">
+      <h1>Carni Preferite</h1>
+      <ul>
+        {favourites.map((carne) => (
+          <li key={carne.id} className="favourite-item">
+            <Link to={`/details/${carne.id}`}>{carne.title}</Link>
+            <button onClick={() => removeFavourite(carne.id)}>Rimuovi</button>
+          </li>
+        ))}
+      </ul>
+      <Link to="/">Torna alla lista</Link>
+    </div>
+  );
+}
+
 export default Favourites;
